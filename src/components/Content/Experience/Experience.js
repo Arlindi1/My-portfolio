b75ea93f61b179314bd1154dde24299fa0e7bacd
Arlindi1 <arlind1arifaj@gmail.com>
2024-09-10 14:52:53 +0200

    Updated the company logo picture links

--- src/components/Content/Experience/Experience.js
+++ src/components/Content/Experience/Experience.js
@@ -5,7 +5,7 @@ const experiences = [
   {
     title: 'Student Participant - Advanced JavaScript and React',
     company: 'Beetroot Academy',
-    imageUrl: 'https://scontent.fprn1-1.fna.fbcdn.net/v/t39.30808-6/276030875_2115637128622774_1931392917098148295_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=pYk0ryi-C1wQ7kNvgFC5pAW&_nc_ht=scontent.fprn1-1.fna&oh=00_AYD7Q9hUvdh-zRYnXoZSUrMSOftUTgoBoxan11829EOAAg&oe=66DCBA97',
+    imageUrl: 'https://d92mrp7hetgfk.cloudfront.net/images/sites/misc/beetroot_resized/original.png?1664306157',
     period: 'Jan 24 - Mar 24 ',
     description: 'Beetroot Academy is a Swedish-Ukrainian technology school that helps people take their careers to the next level. We offer intensive IT courses in a friendly atmosphere where no one is left behind. The Beetroot Academy in Pristina is managed by the company Kutia.',
     tags: ['JavaScript', 'React'],
@@ -14,7 +14,7 @@ const experiences = [
   {
     title: 'Student Participant - Web Development',
     company: 'Makerminds',
-    imageUrl: 'https://scontent.fprn1-1.fna.fbcdn.net/v/t39.30808-1/356426914_760252502769148_5933416695238499335_n.png?stp=dst-png_s200x200&_nc_cat=110&ccb=1-7&_nc_sid=f4b9fd&_nc_ohc=dDOcFF2sNKsQ7kNvgG0afuo&_nc_ht=scontent.fprn1-1.fna&oh=00_AYC78FsTuRqrPYyiMeqMSN71WnZvlzImnnnpEOLyNEQzxw&oe=66DCCCDA',
+    imageUrl: 'https://scontent.fprn1-1.fna.fbcdn.net/v/t39.30808-6/356426914_760252502769148_5933416695238499335_n.png?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=mN3GTe_Fk08Q7kNvgFIskJd&_nc_ht=scontent.fprn1-1.fna&_nc_gid=AkZxoqOVRwhuiqwT3kSeerM&oh=00_AYAwXudkspJAieKPIJ28XWcrbP6dYYC9r2N8ugknE3OFjw&oe=66E62AE0',
     period: 'Nov 23 - Jan 24 ',
     description: 'Intensive training course covering the basics of web development using HTML, CSS, and JavaScript. This course transforms participants from beginners to capable developers within 3 months, advancing their knowledge and preparing them to meet the demands of the modern digital era as web developers.',
     tags: ['HTML', 'CSS', 'JavaScript'],
