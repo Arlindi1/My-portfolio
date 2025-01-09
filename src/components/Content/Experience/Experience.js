e9bf6365344d4b3aaab797814f522cb17f522fb0
Arlindi1 <arlind1arifaj@gmail.com>
2025-01-09 13:53:53 +0100

    Updated the experieence section

--- src/components/Content/Experience/Experience.js
+++ src/components/Content/Experience/Experience.js
@@ -6,7 +6,7 @@ const experiences = [
     title: 'Frontend Intern - Sigal Uniqa',
     company: 'Sigal UNIQA',
     imageUrl: 'https://th.bing.com/th/id/R.a0c347076ded495fdbd55bc443e900d5?rik=Y9zaVDA1%2bFBibg&riu=http%3a%2f%2fsigal.com.al%2fwp-content%2fuploads%2f2019%2f05%2flogo-SIGAL.png&ehk=a5V3moSFNzK%2bswsbJBwDdcctCz9BQ1hqPlgryxe0EaA%3d&risl=&pid=ImgRaw&r=0',
-    period: 'Jul 24 - Oct 24',
+    period: 'Aug 24 - Jan 25',
     description: 'As a Frontend Web Developer at Sigal Uniqa, I work on creating intuitive and user-friendly web interfaces for our insurance platforms. My role involves collaborating with cross-functional teams to develop responsive websites and web applications using technologies like HTML5, CSS3, JavaScript, and modern frameworks such as React. I focus on optimizing web performance, ensuring accessibility compliance, and enhancing the overall user experience to meet both client and business needs.',
     tags: ['JavaScript', 'React', 'Wordpress'],
     link: 'https://sigal-ks.com/' // replace with actual link if available
@@ -23,7 +23,7 @@ const experiences = [
   {
     title: 'Student Participant - Web Development',
     company: 'Makerminds',
-    imageUrl: 'https://media.licdn.com/dms/image/v2/C4D0BAQFGEo_OJBqQgg/company-logo_200_200/company-logo_200_200/0/1630532393024/makerminds_llc_logo?e=1734566400&v=beta&t=YcF6jQrirV3BDolCLiFZAM-dcbiXn2VrJkUgH-9N7ws',
+    imageUrl: 'https://makerminds.de/wp-content/uploads/2021/05/Banner_Logo_MM-300x169.png',
     period: 'Nov 23 - Jan 24 ',
     description: 'Intensive training course covering the basics of web development using HTML, CSS, and JavaScript. This course transforms participants from beginners to capable developers within 3 months, advancing their knowledge and preparing them to meet the demands of the modern digital era as web developers.',
     tags: ['HTML', 'CSS', 'JavaScript'],
